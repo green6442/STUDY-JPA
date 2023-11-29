ec20eb9ccd0e5171b84c461d3a104b6cc8e35556
subin <green6442278@gmail.com>
2023-11-29 11:21:51 +0900

    다양한 연관 관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery") // 어떤 주문에 의해 배송이 되는 지 알고싶어
+    private Order order;
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String city;
+    private String name;
+    private String street;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member") // 연관 관계 주인 설정(외래키)
+    private List<Order> orders = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+}
